fe0d0fbc39520168ca044e15a28dd8aaecd6797a
Lorunser <ltray99@gmail.com>
2018-04-04 17:34:54 +0100

    Fix navbar and popper.js

--- Studion/Global.asax.cs
+++ Studion/Global.asax.cs
@@ -6,9 +6,6 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 
-//3rd party
-using GleamTech.DocumentUltimate.Web;
-
 namespace Studion
 {
     public class MvcApplication : System.Web.HttpApplication
@@ -19,10 +16,6 @@ namespace Studion
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
-
-            //The default CachePath value is "~/App_Data/DocumentCache"
-            //Both virtual and physical paths are allowed.
-            DocumentUltimateWebConfiguration.Current.CachePath = "~/App_Data/DocumentCache";
         }
     }
 }
