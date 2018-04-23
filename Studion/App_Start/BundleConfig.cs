2121d65ea031962ce79d8603bd0149b01e4a38b1
Lorunser <ltray99@gmail.com>
2018-04-23 11:56:03 +0100

    Misc deployment changes

--- Studion/App_Start/BundleConfig.cs
+++ Studion/App_Start/BundleConfig.cs
@@ -9,11 +9,14 @@ namespace Studion
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js",
+                        //"~/Scripts/jquery-{version}.js", //removed as server does not like wildcards
+                        "~/Scripts/jquery-3.3.1.js",
                         "~/Scripts/jquery.dataTables.min.js")); // addition
-
+            /*
+             * //removed because of wildcard
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+                        */
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
