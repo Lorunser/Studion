1dd8994d9783d8845f0bcfb1b09ea2e026195746
Lorunser <ltray99@gmail.com>
2018-08-28 20:35:30 +0200

    Prevent xss in comment form

--- Studion/Scripts/Notes/display.js
+++ Studion/Scripts/Notes/display.js
@@ -57,7 +57,7 @@ function commentList(noteID, username) {
                                 "</p>\
                                 <div class='row'>\
                                 <p class='col-md-11'>" +
-                                comment.commentMessage +
+                                htmlEncode(comment.commentMessage) +
                                 "</p>" +
                                 deleteButton +
                                 "</div>\
@@ -112,7 +112,7 @@ function commentForm(noteID, username) {
                                 "</p>\
                                 <div class='row'>\
                                 <p class='col-md-11'>" +
-                                commentDto.commentMessage +
+                                htmlEncode(commentDto.commentMessage) +
                                 "</p>" +
                                 deleteButton +
                                 "</div>\
@@ -155,4 +155,25 @@ function commentDeleteButtons() {
             }
         })
     });
+}
+
+//xss prevention functions
+function htmlEncode(str) {
+    // This must be done in a proper order to achive desired results.
+    str = this.replaceAll(str, "&", "&amp;");
+    str = this.replaceAll(str, "<", "&lt;");
+    str = this.replaceAll(str, ">", "&gt;");
+    str = this.replaceAll(str, " ", "&nbsp;");
+    return str;
+}
+
+function replaceAll(str, subStr, newStr) {
+    var offset = 0;
+    var index = str.indexOf(subStr);
+    while (index != -1) {
+        str = str.substr(0, index) + newStr + str.substr(index + subStr.length);
+        offset = index + newStr.length;
+        index = str.indexOf(subStr, offset);
+    }
+    return str;
 }
